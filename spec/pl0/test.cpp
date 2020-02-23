c25e13fc471bbbee87ae3df077309e438f9dc402
01nomagic <784694610@qq.com>
2020-02-23 10:44:44 +0800

    adjust test structure

--- spec/pl0/test.cpp
+++ spec/pl0/test.cpp
@@ -1,13 +1,11 @@
 #include <cassert>
 #include <string>
 
-#include "utils/utils.hpp"
-#include "lex/lex.hpp"
-#include "syntax/LR1.hpp"
+#include "utils.hpp"
+#include "lex.hpp"
+#include "LR1.hpp"
 
-#include "spec/pl0/pl0.hpp" // 解析PL/0语言
-//#include "spec/()/().hpp"  // 解析括号语言
-//#include "spec/01/01.hpp" // 解析自定义的语言
+#include "pl0.hpp"
 
 using namespace std;
 
@@ -15,10 +13,9 @@ using My_Lex = WrapLex<Symbol>; // 生成PL/0词法分析器的class
 using My_LR1 = LR1<Symbol, My_Lex>; // 生成PL/0 LR(1)语法分析器的class
 
 int main (int argc, char *argv[]) {
-  assert(argc >= 2);
-
+  string sourceFile = "test1.pl0";
   printNow();
-  My_Lex lex(LEX, string(argv[1])); // 第二个参数为需要编译的源码文件路径
+  My_Lex lex(LEX, sourceFile); // 第二个参数为需要编译的源码文件路径
 //  cout << lex.getAllToken();
   My_LR1 lr1(CFG, NonterminalSymbolType::Program, lex);
   printNow();
